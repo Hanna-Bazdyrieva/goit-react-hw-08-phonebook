45386a4819e30766332952ff5a53380fc09513a4
Hanna Bazdyrieva <119747532+Hanna-Bazdyrieva@users.noreply.github.com>
2023-04-16 15:06:29 +0200

    adds basename

--- src/index.js
+++ src/index.js
@@ -10,9 +10,11 @@ import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
+// var baseUrl = window.location.origin;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
       <ThemeProvider theme={theme}>
         <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
